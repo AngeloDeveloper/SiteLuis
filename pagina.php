3dc0cf68c820f3cd2d721af643802323397f7056
Angelo Gabriel Silva Ribeiro <angelogabrielsilvaribeiro@gmail.com>
2018-09-15 12:43:00 -0300

    Demonstracao
    uma simples demonstracao do site

---
+++ pagina.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML>
+<!--
+	Hielo by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<title>Site para Luís</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<script>
+	$(document).ready(function(){
+    $("button").click(function(){
+        $("p").toggle();
+    });
+	});
+</script>
+	</head>
+	<body class="subpage">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="logo"><a href="index.html">Gerador<span>de números</span></a></div>
+			</header>
+
+
+
+		<!-- One -->
+			<section id="One" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>Sorte? Aposte com inteligência.</p>
+						<h2>Realize seu sonho</h2>
+					</header>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style2">
+				<div class="inner">
+					<div class="box">
+						<!--conteudoaqui-->
+					<div>
+						<!-- Form -->
+								<h3>Quantidade de grupos</h3>
+
+								<form method="post" action="#">
+									<div class="row uniform">
+										<div class="6u 12u$(xsmall)">
+											<input type="text" name="name" id="name" value="" placeholder="100" />
+										</div>
+										<div class="12u$">
+											<h3>os números vão de 0 até qual número??</h3>
+												<div class="6u 12u$(xsmall)">
+												<input type="text" name="name" id="name" value="" placeholder="60" />
+												</div>
+											</div>
+										</div>
+										
+										<!-- Break -->
+										<div class="12u$">
+											<h3>Quantidade de números</h3>
+											<div class="select-wrapper">
+												<select name="category" id="category">
+													<option value="5">05 números</option>
+													<option value="6">06 números</option>
+													<option value="7">07 números</option>
+													<option value="8">08 números</option>
+													<option value="9">09 números</option>
+													<option value="10">10 números</option>
+													<option value="11">11 números</option>
+													<option value="12">12 números</option>
+													<option value="13">13 números</option>
+													<option value="14">14 números</option>
+													<option value="15">15 números</option>
+													<option value="16">16 números</option>
+													<option value="17">17 números</option>
+													<option value="18">18 números</option>
+													<option value="19">19 números</option>
+													<option value="20">20 números</option>
+												</select>
+											</div>
+										</div>
+										<div class="4u 12u$(small)">
+											<input type="radio" id="priority-low" name="priority" onclick="">
+											<label for="priority-low">Fixar dezena</label>
+											
+										</div>
+										<p>This is a paragraph with little content.</p>
+											<p>This is another small paragraph.</p>
+
+										<button>Toggle between hiding and showing the paragraphs</button>
+										<div class="6u 12u$(xsmall)">
+												<input type="text" name="name" id="ocultar" value="" placeholder="60" />
+											</div>
+										<!-- Break -->
+										<!-- Break -->
+										<!-- Break -->
+										
+										<!-- Break -->
+										<div class="12u$">
+											<ul class="actions">
+												<li><input type="submit" value="Gerar números" /></li>
+												<li><input type="reset" value="Reset" class="alt" /></li>
+											</ul>
+										</div>
+									</div>
+								</form>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Untitled. All rights reserved.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
